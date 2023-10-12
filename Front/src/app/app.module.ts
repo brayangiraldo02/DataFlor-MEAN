03e365ca04d47561421bac37ebe3355f5ac44cee
Brayan Giraldo <b.catano@utp.edu.co>
2023-10-11 23:22:48 -0500

    update file location

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
