18c228ed7ec74b85a1ecda79a0b080bab5ecf779
Brayan Giraldo <b.catano@utp.edu.co>
2023-10-22 02:35:12 -0500

    Database connection successful

---
+++ Back/config/db/database.js
@@ -0,0 +1,29 @@
+import Sequelize from 'sequelize';
+
+// Conexión a la base de datos DataFlor en PostgreSQL hecha con sequelize
+const sequelize = new Sequelize(
+  "DataFlor",
+  "postgres",
+  "123456789",
+  {
+    host: "localhost",
+    dialect: 'postgres',
+    port: 5432,
+    define: {
+      timestamps: false,
+    },
+  },
+);
+
+// Prueba de conexión a la base de datos
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.log(dbConfig);
+    console.log('Unable to connect to the database:', err);
+  });
+
+export default sequelize;
