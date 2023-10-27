466af966bd4961a3f200ff79eb23ff2f62c2f20f
Brayan Giraldo <b.catano@utp.edu.co>
2023-10-26 22:26:24 -0500

    Provider component

--- Front/src/app/components/create-provider/create-provider.component.ts
+++ Front/src/app/components/create-provider/create-provider.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-provider',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-provider.component.css']
 })
 export class CreateProviderComponent {
+  provider: any = {
+    fullname: '',
+    address: '',
+    phone: ''
+  };
+  
+  constructor(private http: HttpClient, private router: Router) {}
 
+  // Método para enviar los datos al backend y crear un proveedor.
+  createProvider() {
+    this.http.post('http://localhost:5000/providers/create', this.provider)
+      .subscribe(
+        (data: any) => {
+          // Manejar respuesta del servidor si es exitosa.
+          alert('Proveedor creado exitosamente');
+          this.router.navigate(['/providers']);
+        },
+        (error) => {
+          // Manejar error del servidor.
+          alert('Error al crear el proveedor');
+        }
+      );
+  }
 }
