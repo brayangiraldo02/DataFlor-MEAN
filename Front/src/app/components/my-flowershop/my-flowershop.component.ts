e245ce8366ae31d787b402393c84814082101e07
SMejia115 <s.mejia6@utp.edu.co>
2023-10-26 20:44:08 -0500

    Create my-flowershop component.

--- Front/src/app/components/my-flowershop/my-flowershop.component.ts
+++ Front/src/app/components/my-flowershop/my-flowershop.component.ts
@@ -10,32 +10,54 @@ import { OnInit } from '@angular/core';
   styleUrls: ['./my-flowershop.component.css']
 })
 export class MyFlowershopComponent implements OnInit{
-  flowershopId: any;
+  flowershopId: any = 0;
+  flowershopData: any = {};
+  inEditMode: boolean = false;
+
+
+  enterEditMode() {
+    this.inEditMode = true;
+  }
 
   constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.flowershopId = this.getOwnerInfo();
+    this.getOwnerInfo();
     this.getFlowershop();
   }
 
-  getOwnerInfo(): any {
+  getOwnerInfo(){
     const token: any = localStorage.getItem('token');
     const tokenDesencripted: any = jwtDecode(token);
-    console.log(tokenDesencripted.idFlowershops);
-    return tokenDesencripted.idFlowershops;
+    this.flowershopId = tokenDesencripted.user.idflowershops;
   }
 
   getFlowershop(): void {
     this.http.get(`http://localhost:5000/flower-shops/${this.flowershopId}}`).subscribe((data: any) => {
-      console.log(data);
+      this.flowershopData = data;
+      console.log(this.flowershopData);
     }, error => {
-      console.log(error);
+      window.alert("ID -->" + this.flowershopId);
     }
     );
   }
 
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
 
+  updateFlowershop(): void {
+    this.http.put(`http://localhost:5000/flower-shops/update/id/${this.flowershopId}`, this.flowershopData).subscribe((data: any) => {
+      this.flowershopData = data;
+      console.log(this.flowershopData);
+      window.alert("Flower shop updated successfully");
+      this.getFlowershop();
+      this.inEditMode = false;
+    }, error => {
+      window.alert("ID -->" + this.flowershopId);
+    }
+    );
+  }
 
 
 
