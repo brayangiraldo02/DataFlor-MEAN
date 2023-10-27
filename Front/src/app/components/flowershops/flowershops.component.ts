502da1da9f13ada71541d90025992b90d12c33f1
Brayan Giraldo <b.catano@utp.edu.co>
2023-10-27 02:08:50 -0500

    Validators

--- Front/src/app/components/flowershops/flowershops.component.ts
+++ Front/src/app/components/flowershops/flowershops.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; // Importa Validators y FormGroup
 
 @Component({
   selector: 'app-flowershops',
   templateUrl: './flowershops.component.html',
   styleUrls: ['./flowershops.component.css'] // Agrega el archivo CSS si lo tienes
 })
+
 export class FlowershopsComponent implements OnInit {
   flowerShops: any[] = []; // Arreglo para almacenar las floristerías
   selectedFlowerShop: any; // Floristería seleccionada para edición
   isEditing: boolean = false; // Variable para controlar la edición
+  flowerShopForm!: FormGroup; // Define el FormGroup
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.getFlowerShops();
@@ -38,11 +41,22 @@ export class FlowershopsComponent implements OnInit {
   editFlowerShop(flowerShop: any) {
     this.selectedFlowerShop = { ...flowerShop };
     this.isEditing = true;
+
+    // Inicializar el formulario con los valores de la floristería seleccionada
+    this.flowerShopForm = this.formBuilder.group({
+      fullname: [this.selectedFlowerShop.fullname, [Validators.required, Validators.pattern('[a-zA-Z ]*')]],
+      address: [this.selectedFlowerShop.address, Validators.required],
+      phone: [this.selectedFlowerShop.phone, [Validators.required, Validators.pattern('^[0-9]*$')]],
+      state: [this.selectedFlowerShop.state, Validators.required],
+    });
   }
 
   // Guardar los cambios en la floristería
   saveChanges() {
-    if (this.selectedFlowerShop) {
+    if (this.selectedFlowerShop && this.flowerShopForm.valid) {
+      // Actualizar los valores de la floristería seleccionada con los del formulario
+      this.selectedFlowerShop = { ...this.selectedFlowerShop, ...this.flowerShopForm.value };
+      
       this.http.put(`http://localhost:5000/flower-shops/update/id/${this.selectedFlowerShop.idflowershops}`, this.selectedFlowerShop)
         .subscribe((data: any) => {
           // Actualizar la lista de floristerías después de la edición
